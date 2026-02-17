edd163f7bf97bb18d9901896f9aff28b2a9de904
Ethan Gaddis <ethan@arnica.io>
2026-02-17 08:47:58 -0500

    Testing this out

---
+++ test.java
@@ -0,0 +1,17 @@
+public class AESEncruptDecryptMACCUtility {
+
+    /** The Constant Algo. */
+    private static final String ALGO   = "AES";
+
+    public static final String NOSPACE   ="";
+
+    /** The Constant keyValue. */
+    private static final byte[] KEYVALUE = new byte[] { 'T', 'h', 'e', 'B', 'e', 's', 't', 'S', 'e', 'c', 'r', 'e', 't', 'K', 'e', 'y' };
+
+    /**
+     * Instantiates a new AES encrypt decrypt utility.
+     */
+    private AESEncruptDecryptMACCUtility() {
+        // this class cannot be instantiated
+    }
+}
