89c27b3ef9caf9fe770098a370aef63a88279d8d
Ethan Gaddis <ethan@arnica.io>
2026-02-18 14:22:48 -0500

    Add file enumeration with prefix filtering
    Implement file enumeration and filtering based on prefix.

---
+++ FileTest.cs
@@ -0,0 +1,19 @@
+var validatedPath = ValidatePathWithinDirectory(fs.AppDataDirectory, filesSubDirectory);
+
+            //TODO: This code searches for a string, would me more efficient as a direct search
+            await Task.Run(() =>
+            {
+                foreach (string filePath in Directory.EnumerateFiles(validatedPath, $"*.{fileExtensionNoPeriod}"))
+                {
+                    var fileNameNoPath = string.Empty;
+                    if (filePath != null)
+                    {
+                        var lastSlash = filePath.LastIndexOf("/");
+
+                        if (lastSlash > -1)
+                        {
+                            fileNameNoPath = filePath.Substring(lastSlash + 1, filePath.Length - (lastSlash + 1));
+                        }
+
+                        if (fileNameNoPath.StartsWith(fileNamePrefixFilter))
+                            fileList.Add(File.ReadAllText(filePath));
